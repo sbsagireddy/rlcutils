6f64a74e12290ee62682e18764cedbd30d69ebb5
sbsagireddy <sbsagireddy@gmail.com>
2024-02-08 12:19:44 -0800

    update _version

--- rlcutils/_version.py
+++ rlcutils/_version.py
@@ -1,6 +1,6 @@
 """Contains the version information for rlcutils."""
 # major, minor, patch
-version_info = 0, 1, 0
+version_info = 0, 1, 1
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
